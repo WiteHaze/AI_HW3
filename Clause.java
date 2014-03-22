4ee3f3130386d3a5998e612130bac85a5afe1d8b
Anthony Tran <anthony.wind5@gmail.com>
2014-03-21 20:03:20 -0500

    Basic resolution. Repeating literals unchecked. Working test case.

--- Clause.java
+++ Clause.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 class Clause
 {
-	int ID; //ID of this clause. Would be the order of the clause in the input file
-	String[] literal; //list of literals in this clause
-	boolean[] negated; //negated[i] is true when literal[i] is negated
-	int[] parents = {-1, -1}; //if this clause was derived, then this field would have non -1 parents
+	private int ID; //ID of this clause. Would be the order of the clause in the input file
+	private String[] literal; //list of literals in this clause
+	private boolean[] negated; //negated[i] is true when literal[i] is negated
+	private int[] parents = {-1, -1}; //if this clause was derived, then this field would have non -1 parents
 
 	public Clause(int ID, String[] literal, boolean[] negated)
 	{
@@ -22,11 +22,58 @@ class Clause
 		this.parents = parents;
 	}
 	
-	//Takes in another clause and if it can be resolved, then it will add the produced clause to the clauseList
-	/*
+	//Takes in another clause and if it can be resolved, then it will add the produced clause to the clauseList. Returns true if there was no contradiction. False if there was.
 	public boolean resolution(Clause otherClause, LinkedList<Clause> clauseList)
 	{
+		for(int i = 0; i < literal.length; i++) //Loop through all the literals in this clause
+		{
+			for(int j = 0; j < otherClause.literal.length; j++) //Loop through all the literals in the otherClause
+			{
+				if(literal[i].equals(otherClause.literal[j]) && negated[i] != otherClause.negated[j]) //if the literals are the same and the negation is different, then they can be resolution'd
+				{
+					if(literal.length == 1 && otherClause.literal.length == 1) //If boths lengths are 1, then there is a contradiction
+					{
+						return false;
+					}
 
+					
+					//Add the literals from both clauses to the new clause
+					String[] newLiteral = new String[literal.length + otherClause.literal.length - 2]; //the new clause will have literals of both minus the removed literal from both
+					boolean[] newNegated = new boolean[negated.length + otherClause.negated.length - 2];
+					int newLiteralIndex = 0;
+
+					for(int k = 0; k < literal.length; k++)
+					{
+						if(k != i) //k is not the index of the removed literal
+						{
+							newLiteral[newLiteralIndex] = literal[k];
+							newNegated[newLiteralIndex] = negated[k];
+							newLiteralIndex++;
+						}
+					}
+					for(int k = 0; k < otherClause.literal.length; k++)
+					{
+						if(k != j)
+						{
+							newLiteral[newLiteralIndex] = otherClause.literal[k];
+							newNegated[newLiteralIndex] = otherClause.negated[k];
+							newLiteralIndex++;
+						}
+					}
+					String outputStr = "";
+					for(int k = 0; k < newLiteral.length; k++)
+					{
+						if(newNegated[k])
+						{
+							outputStr += "~";
+						}
+						outputStr += (newLiteral[k] + " ");
+					}
+					System.out.println(outputStr);
+				}
+			}
+		}
+
+		return true;
 	}
-	*/
 }
--- Resolution.java
+++ Resolution.java
@@ -14,5 +14,14 @@ class Resolution
 		//Example, before: <1 2 3>, after <1 2 3 4>. We would add 4 to the stack. Assuming 4 was added after resolving clause 3, the stack would be <4 2 1>, so we would resolve 4 next.
 		//We stop when the resolution function returns "false", which means that the resolution found contradicting clauses. Otherwise, if the loop continues to completion, then every clause has been tested.
 		//At this point, we return failure.
+
+		String[] testString1 = {"z", "y", "x"};
+		String[] testString2 = {"y", "w"};
+		boolean[] testBool1 = {true, false, true};
+		boolean[] testBool2 = {true, false};
+		Clause testClause1 = new Clause(1, testString1, testBool1);
+		Clause testClause2 = new Clause(2, testString2, testBool2);
+		testClause1.resolution(testClause2, clauses); 
+		System.out.println("End");
 	}
 }
